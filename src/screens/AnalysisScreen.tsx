4db24e54e1aea14a224c69bf7f4f6ef80dcc51d5
jyx0615 <jyx920615@gmail.com>
2025-03-08 21:28:03 +0800

    Update duration selector

--- src/screens/AnalysisScreen.tsx
+++ src/screens/AnalysisScreen.tsx
@@ -1,53 +1,73 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, StatusBar} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
 import Layout from '../components/Layout';
-import SwitchSelector from 'react-native-switch-selector';
-import DurationSelector from '../components/DurationSelector';
 import ExpensePieChart from '../components/ExpensePieChart';
-import {colors} from '../theme/colors';
-// import DateTimePickerModal from "react-native-modal-datetime-picker";
+import LeftTriangle from '../svg/LeftTriangle';
+import RightTriangle from '../svg/RightTriangle';
 
-// import ExpensePieChart from '../components/ExpensePieChart';
+interface RadioButtonProps {
+  label: string;
+  selected: boolean;
+  onPress: () => void;
+}
+
+const RadioButton = ({label, selected, onPress}: RadioButtonProps) => {
+  return (
+    <TouchableOpacity
+      style={[styles.radioButton, selected && styles.selectedButton]}
+      onPress={onPress}>
+      <Text style={styles.label}>{label}</Text>
+    </TouchableOpacity>
+  );
+};
 
 const AnalysisScreen = () => {
-  const [selectedButton, setSelectedButton] = useState<number>(2);
+  const [selectedButton, setSelectedButton] = useState<String>('收入');
+  const [selectedOption, setSelectedOption] = useState<String>('Option 1');
+  const options = ['收入', '支出', '結餘'];
+  const duration = ['一個月', '半年', '一年', '自訂'];
 
-  const handleButtonPress = (buttonIndex: number) => {
-    setSelectedButton(buttonIndex);
+  const handleButtonPress = (type: String) => {
+    setSelectedButton(type);
     console.log('selectedButton:', selectedButton);
   };
-
-  const options = [
-    {label: '收入', value: '1'},
-    {label: '支出', value: '2'},
-    {label: '結餘', value: '3'},
-  ];
 
   return (
     <Layout>
       <StatusBar barStyle="dark-content" backgroundColor="#fff" />
       <View style={styles.content}>
-        <SwitchSelector
-          initial={0}
-          textColor="#000000"
-          borderColor="black"
-          buttonColor="#BCD635"
-          selectedColor="white"
-          backgroundColor={colors.background}
-          hasPadding
-          borderRadius={10}
-          options={options}
-          onPress={(value: number) => handleButtonPress(value)}
-          style={styles.switchSelector}
-        />
-        <View style={styles.durationSelector}>
-          <Text>查詢範圍：</Text>
-          <DurationSelector />
+        <View style={styles.typeSelector}>
+          {options.map((option, index) => (
+            <RadioButton
+              key={index}
+              label={option}
+              selected={selectedButton === option}
+              onPress={() => handleButtonPress(option)}
+            />
+          ))}
+        </View>
+
+        <View style={styles.durationContainer}>
+          {duration.map((option, index) => (
+            <RadioButton
+              key={index}
+              label={option}
+              selected={selectedOption === option}
+              onPress={() => setSelectedOption(option)}
+            />
+          ))}
         </View>
 
         <View style={styles.durationSelector}>
-          <Text>開始日期：</Text>
-          <DurationSelector />
+          <LeftTriangle width={20} height={20} />
+          <Text>自訂日期</Text>
+          <RightTriangle width={20} height={20} />
         </View>
 
         <View style={styles.chart}>
@@ -67,18 +87,70 @@ const styles = StyleSheet.create({
   switchSelector: {
     width: '50%',
     alignSelf: 'center',
-    marginTop: 20,
+    marginTop: 5,
     marginBottom: 10,
   },
   durationSelector: {
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '95%',
     marginTop: 10,
     marginBottom: 10,
   },
   chart: {
     marginTop: 10,
     width: '90%',
+  },
+  circle: {
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: '#555',
+    marginRight: 10,
+  },
+  selectedCircle: {
+    backgroundColor: '#007AFF',
+    borderColor: '#007AFF',
+  },
+  label: {
+    fontSize: 16,
+    paddingVertical: 3,
+    alignContent: 'center',
+  },
+  radioButton: {
+    width: '33%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '99%',
+  },
+  selectedButton: {
+    backgroundColor: '#BCD635',
+  },
+  durationContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignContent: 'center',
+    width: '95%',
+    marginTop: 10,
+    borderTopColor: '#555',
+    borderTopWidth: 1,
+    borderBottomColor: '#555',
+    borderBottomWidth: 1,
+    padding: 0,
+  },
+  typeSelector: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignContent: 'center',
+    width: '50%',
+    marginTop: 10,
+    padding: 0,
+    borderColor: '#555',
+    borderWidth: 1,
+    borderRadius: 5,
   },
 });
 
